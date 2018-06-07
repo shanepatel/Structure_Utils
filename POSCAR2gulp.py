ef42376b0fe14a25eb704459de06aaf877fbabe5
Shane <shane@dhcp-10-105-85-248.wireless.northwestern.private>
2018-06-07 15:36:08 -0500

    Committer: Shane <shane@dhcp-10-105-85-248.wireless.northwestern.private>
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   POSCAR2gulp.py

--- POSCAR2gulp.py
+++ POSCAR2gulp.py
@@ -1,13 +1,17 @@
 import vasp
 import numpy as np
 import pickle as pkl
+import glob
 def main(filein,system='STO',MHM=True):
     pots = pkl.load(open('potdict.p', 'rb'))
     atoms = vasp.poscar_read(filein)
     fileout = filein.replace('.vasp','.in')
     cellvec = '  '+str(atoms.cellvec[0][0])+' '+str(atoms.cellvec[1][1])+' '+str(atoms.cellvec[2][2])+' 90 90 90\n'
     f = open(fileout,'w')
-    f.write('socket opti dist conp\n gtol 0.0005\n gmax opt 0.0000005\n maxcyc 10000000\nsocket host\nsto_XXX/\nsocket inet 0\n')
+    if MHM == True and system =='STO':
+        f.write('socket opti dist conp\n gtol 0.0005\n gmax opt 0.0000005\n maxcyc 10000000\nsocket host\nsto_XXX/\nsocket inet 0\n')
+    else if MHM = False:
+        f.write('single\n')
     if system == 'STO':
         f.write('cutp 20.0 polynomial 18.0\n  cell\n')
     f.write(cellvec)
@@ -17,4 +21,5 @@ def main(filein,system='STO',MHM=True):
         f.write(linetext)
     f.write('\n'+pots['mccoy']+'\n')
     f.close()
+poscars=glob.glob('*.vasp')
 main('POSCAR.vasp')
